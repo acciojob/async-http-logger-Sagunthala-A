457680b53ba5128c91eabe74817d787ecdd9daf6
Sagunthala A <117196512+Sagunthala-A@users.noreply.github.com>
2024-08-13 23:09:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 async function fetchingApi(){
 	try{
 		const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
-		console.log(res);
+		console.log(res.body);
 	}
 	catch(e){
 		console.log(e)
